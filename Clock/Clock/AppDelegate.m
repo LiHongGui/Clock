d66013ecbe8985b3dec7cc879876ebe02db94559
lihonggui1988@outlook.com <lihonggui1988@outlook.com>
2016-12-13 10:54:21 +0800

    1.开启后,屏幕变暗(支持常亮) 2.退出,屏幕亮度还原

--- Clock/Clock/AppDelegate.m
+++ Clock/Clock/AppDelegate.m
@@ -9,7 +9,7 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
+@property(nonatomic,assign) CGFloat bright;
 @end
 
 @implementation AppDelegate
@@ -17,6 +17,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //获取屏幕亮度
+    CGFloat bright = [UIScreen mainScreen].brightness;
+    _bright = bright;
+    [[UIScreen mainScreen] setBrightness:0.1];
+     [[UIApplication sharedApplication] setIdleTimerDisabled:YES];
     return YES;
 }
 
@@ -24,27 +29,34 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+     [[UIScreen mainScreen] setBrightness:self.bright];
 }
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[UIScreen mainScreen] setBrightness:self.bright];
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    [[UIScreen mainScreen] setBrightness:0.1];
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [[UIScreen mainScreen] setBrightness:0.1];
+
+    
 }
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+     [[UIScreen mainScreen] setBrightness:self.bright];
 }
 
 
--- Clock/Clock/ViewController.m
+++ Clock/Clock/ViewController.m
@@ -11,6 +11,8 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property(nonatomic,strong) UIView *viewSecond;
+@property(nonatomic,strong) UIView *viewMinute;
+@property(nonatomic,strong) UIView *viewHour;
 @end
 
 @implementation ViewController
@@ -22,14 +24,31 @@
 //    [self.view.layer addSublayer:layer];
     CGPoint centerPoint = self.imageView.center;
     
-    //设置指针
-    UIView *viewSecond = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 1, 100)];
+    //设置时针
+    UIView *viewHour = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 5, 70)];
+    _viewHour = viewHour;
+    viewHour.center = self.imageView.center;
+    viewHour.layer.anchorPoint = CGPointMake(0.5, 1);
+    [viewHour setBackgroundColor:[UIColor blueColor]];
+    [self.view addSubview:viewHour];
+    //设置分针
+    UIView *viewMinute = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 3, 90)];
+    _viewMinute = viewMinute;
+    viewMinute.layer.anchorPoint = CGPointMake(0.5, 1);
+    [viewMinute setBackgroundColor:[UIColor blackColor]];
+    viewMinute.center = self.imageView.center;
+    [self.view addSubview:viewMinute];
+
+    //设置秒针
+    UIView *viewSecond = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 1, 120)];
     _viewSecond = viewSecond;
     viewSecond.layer.anchorPoint = CGPointMake(0.5,1);
     viewSecond.center = centerPoint;
     [viewSecond setBackgroundColor:[UIColor redColor]];
     [self.view addSubview:viewSecond];
-    [self fire];
+    
+    
+    
     
     //定时器:1
 //    NSTimer *timer = [NSTimer timerWithTimeInterval:1 target:self selector:@selector(fire) userInfo:nil repeats:YES];
@@ -49,16 +68,30 @@
     //获取当前时间
     NSDate *currentDate = [NSDate date];
     NSLog(@"currentDate%@",currentDate);
-    //截取一部分时间
-    NSDateComponents *components = [calender components:NSCalendarUnitSecond fromDate:currentDate];
-    NSInteger second = components.second;
-    
+    //截取一部分时间----秒
+    NSDateComponents *componentSecond = [calender components:NSCalendarUnitSecond fromDate:currentDate];
+    NSInteger second = componentSecond.second;
+    //截取一部分时间----分
+    NSDateComponents *componentMinute = [calender components:NSCalendarUnitMinute fromDate:currentDate];
+    NSInteger minute = componentMinute.minute;
+    //截取一部分时间----小时
+    NSDateComponents *componentHour =[calender components:NSCalendarUnitHour fromDate:currentDate];
+    NSInteger hour = componentHour.hour;
+    /*******秒******/
     //设置弧度
+    CGFloat radianSecond = 2*M_PI/60.0;
+    CGFloat radianMinute = 2*M_PI/60.0;
+    CGFloat radianHour = 2*M_PI/12.0;
     //转换一秒=多少弧度
-    CGFloat radian =  2*M_PI/60;
-    radian = radian*second;
-    self.viewSecond.transform = CGAffineTransformMakeRotation(radian);
-    
+   CGFloat radianSec = radianSecond*second;
+    self.viewSecond.transform = CGAffineTransformMakeRotation(radianSec);
+    /*******分******/
+    CGFloat radianMin = minute*radianMinute+ radianSecond/60.0;
+//    CGFloat radianMinute = radian*60*60;
+    self.viewMinute.transform = CGAffineTransformMakeRotation(radianMin);
+    /*******小时******/
+    CGFloat radianHou = hour *radianHour+ radianMin/12;
+    self.viewHour.transform = CGAffineTransformMakeRotation(radianHou);
     
 }
 
